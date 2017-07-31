d0a4d03c3e9a64f703286461e9457004790d55f6
abhishek wani <abhishek wani>
2017-07-31 17:26:57 +0530

    30-03

--- resources/views/profile/manage.blade.php
+++ resources/views/profile/manage.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="row"> 
+         <div class="col-12">
+
+           <nav class="breadcrumb" style="background-color: #fff;">
+              <a class="breadcrumb-item" href="/home">Home</a>
+              <a class="breadcrumb-item" href="/enquiries">Enquiries</a>
+              <span class="breadcrumb-item active">New</span>
+            </nav>
+          
+         </div>
+        </div>
+
     <div class="row justify-content-center">
         <div class="col col-md-8">
             <div class="card">
@@ -15,7 +26,7 @@
                             <label for="name" class="control-label">Name</label>
 
                             
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') ? old('name') : auth()->user()->name }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -29,11 +40,36 @@
                             <label for="email" class="control-label">E-Mail Address</label>
 
                            
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') ? old('email') : auth()->user()->email }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                           
+                        </div>
+
+                        <div class="form-group">
+                            
+                                <button type="submit" class="btn btn-success">
+                                    Update Profile
+                                </button>
+                            
+                        </div>
+
+
+                        <hr>
+
+                        <div class="form-group{{ $errors->has('old_password') ? ' has-error' : '' }}">
+                            <label for="old_password" class="control-label">Password</label>
+
+                           
+                                <input id="old_password" type="old_password" class="form-control" name="old_password" required>
+
+                                @if ($errors->has('old_password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('paold_passwordssword') }}</strong>
                                     </span>
                                 @endif
                            
@@ -63,7 +99,7 @@
                         <div class="form-group">
                             
                                 <button type="submit" class="btn btn-success">
-                                    Create Account
+                                    Update Password
                                 </button>
                             
                         </div>
@@ -72,5 +108,5 @@
             </div>
         </div>
     </div>
-</div>
+
 @endsection
