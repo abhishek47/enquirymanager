fcb645538d7598f5f866fc023856455400001f5f
abhishek wani <abhishek wani>
2017-07-31 21:36:23 +0530

    30-03

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -58,16 +58,43 @@ class HomeController extends Controller
 
     public function updateProfile(Request $request)
     {
+         $this->validate($request, [
+          'name' => 'required',
+          'email' => 'required|email'
+        ]);
+
         auth()->user()->name = $request->get('name');
         auth()->user()->email = $request->get('email');
 
         auth()->user()->save();
 
+        flash('Your Password was updated successfully!')->success();
+
+        return back();
+    }
+    
+    public function password(Request $request)
+    {
+         $this->validate($request, [
+          'old_password' => 'required',
+          'password' => 'required|string|min:6|confirmed'
+        ]);
+
+      $old_password = $request->get('old_password');
+      $password = $request->get('password');
+      if(!\Hash::check($old_password, auth()->user()->password))
+      {
+          flash('Please enter your current password correct!')->error();
+          return back();
+      } 
+
+      auth()->user()->password = bcrypt($password);
+      auth()->user()->save();
+
+      flash('Your Password was updated successfully!')->success();
         return back();
     }
 
-    public function password(Request $request)
-    {
-        return view('profile.manage');
-    }
+
+
 }
