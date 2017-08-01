f3cbb3e849724fddfc994aaab7fea9015f1e7f5c
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-08-01 17:07:25 +0530

    Void Reason

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 <body>
     <div id="app">
     
-    <nav class="navbar navbar-toggleable-md fixed-top navbar-inverse" style="background-color: #3A539B;">
+    <nav class="navbar navbar-toggleable-md fixed-top navbar-inverse" style="background-color: #D91E18;">
      
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
