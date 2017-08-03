b4a6282200f1fcba827884dcaa69db841d2e2ef7
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-08-03 10:46:08 +0530

    Fine

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -61,7 +61,7 @@
                             <div class="form-group{{ $errors->has('start_date') ? ' has-danger' : '' }}">
                                
                                     <label for="start_date" class="control-label">Start Date</label>
-                                    <input id="start_date" type="date" class="form-control{{ $errors->has('start_date') ? ' form-control-danger' : '' }}" name="start_date" value="{{ old('start_date') }}"  autofocus>
+                                    <input id="start_date" type="date" class="form-control{{ $errors->has('start_date') ? ' form-control-danger' : '' }}" name="start_date" value="{{ old('start_date') ? old('start_date') : request('start_date') }}"  autofocus>
 
                                     @if ($errors->has('start_date'))
                                         <span class="form-control-feedback">
@@ -77,7 +77,7 @@
                             <div class="form-group{{ $errors->has('end_date') ? ' has-danger' : '' }}">
                                
                                     <label for="end_date" class="control-label">End Date</label>
-                                    <input id="end_date" type="date" class="form-control {{ $errors->has('end_date') ? ' form-control-danger' : '' }}" name="end_date" value="{{ old('end_date') }}" >
+                                    <input id="end_date" type="date" class="form-control {{ $errors->has('end_date') ? ' form-control-danger' : '' }}" name="end_date" value="{{ old('end_date') ? old('end_date') : request('end_date') }}" >
 
                                     @if ($errors->has('end_date'))
                                         <span class="form-control-feedback">
@@ -107,7 +107,7 @@
                 <div class="col-md-12">
                   <div class="card" >
                        <div class="card-block">
-                           <div class="card-title"><b>Employee Wise Stats</b></div>
+                           <div class="card-title"><b>Employee Wise Stats {{ request('start_date') - request('end_date') }}</b></div>
 
                            <table class="table">
                               <thead class="thead-inverse">
@@ -156,7 +156,7 @@
                 <div class="col-md-12">
                   <div class="card">
                       <div class="card-block">
-                          <div class="card-title"><b>Vehicle Wise Stats</b></div>
+                          <div class="card-title"><b>Vehicle Wise Stats {{ request('start_date') - request('end_date') }}</b></div>
 
                            <table class="table">
                               <thead class="thead-inverse">
@@ -170,15 +170,19 @@
                               <tbody>
                                 
                             
-                            @foreach($vehicles as $index => $vehicles)
+                            @foreach($vehicles as $index => $vehicle)
                             <tr>
                               <td> {{ $index+1 }} </td>
 
-                              <td>{{ $vehicles->name }}</td>
+                              <td>{{ $vehicle->name }}</td>
 
-                              <td>{{ $vehicles->enquiries()->count() }} </td> 
-
-                              <td>{{ $vehicles->enquiries()->where('status', 1)->count() }}</td>
+                              @if(Request::has('start_date') && Request::has('end_date'))
+                                 <td>{{ $vehicle->enquiries()->whereBetween('created_at', [request('start_date'), request('end_date')])->count() }} </td> 
+                                 <td>{{ $vehicle->enquiries()->whereBetween('created_at', [request('start_date'), request('end_date')])->where('status', 1)->count() }}</td> 
+                              @else 
+                               <td>{{ $vehicle->enquiries()->count() }} </td>
+                                <td>{{ $vehicle->enquiries()->where('status', 1)->count() }}</td> 
+                              @endif 
 
                              </tr> 
 
